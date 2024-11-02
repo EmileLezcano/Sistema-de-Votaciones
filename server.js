88a22452ef8b467ee44c4a5f7a892cd00de4e73d
FátimaLezcano <151078699+EmileLezcano@users.noreply.github.com>
2024-11-02 11:07:05 -0300

    agregar comentarios

--- server.js
+++ server.js
@@ -5,26 +5,36 @@ const port = 3000;
 
 app.use(express.json());
 
-//----- Conexión db----------
+//----- Conexión db ----------
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'crud_db',
-    password: '123',
-    port: 5432,
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
 });
 
+//----- Rutas CRUD ----------
+
+// Consulta
 app.get('/temas', async(req, res) =>{
-    const result = await pool.query('SELECT * FROM crud');
-    res.json(result.rows);
+    try{
+        const result = await pool.query('SELECT * FROM crud');
+        res.json(result.rows);
+    } catch {
+        res.status(500).json({message: 'Error al obtener los datos'});
+    }
+    
 });
 
+// Inserción 
 app.post('/temas', async(req, res) =>{
     const {tema, url} = req.body
     const result = await pool.query('INSERT INTO crud (tema, url, voto) VALUES ($1, $2, $3) RETURNING *', [tema,url,0]);
     res.json(result.rows[0]);
 });
 
+// Actualización
 app.put('/temas/:id', async(req, res) =>{
     const id = req.params.id;
     const {tema, url} = req.body;
@@ -33,6 +43,7 @@ app.put('/temas/:id', async(req, res) =>{
     res.json(result.rows[0]);
 });
 
+// Actualización de un parámetro en específico
 app.patch('/temas/:id', async(req, res) =>{
     const id = req.params.id;
     const {voto} = req.body;
@@ -41,12 +52,14 @@ app.patch('/temas/:id', async(req, res) =>{
     res.json(result.rows[0]);
 });
 
+// Eliminación
 app.delete('/temas/:id', async(req, res) => {
     const id = req.params.id;
     const result = await pool.query('DELETE FROM crud WHERE id = $1 RETURNING *', [id]);
     res.json(result.rows[0]);
 });
 
+// Servidor en escucha
 app.listen(port, () =>{
     console.log(`Servidor ejecutandose en http://localhost:${port}`);
 });
